8793511819c24b744aa26b90c586aa905945865a
Weslley Oliveira <weslleyzera2020@gmail.com>
2021-07-19 12:53:01 +0100

    exercice refactory

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,41 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+import '../styles/sidebar.scss';
+
+
+type genres = {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+}
+
+interface SideBarProps {
+  genres: genres[];
+  selectedGenreId: number;
+  set: (id:number) => void;
+}
+
+export function SideBar({genres, selectedGenreId, set } :SideBarProps) {
+  
+  function handleClickButton(id: number) {
+    set(id);
+  }
+
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
